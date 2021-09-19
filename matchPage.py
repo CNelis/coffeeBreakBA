26a45cb7e3e853ee2205b5d3fb067a2bcc91de5c
CNelis <christopher.nelis@yahoo.com>
2021-09-19 12:55:55 +0100

    banner removed

--- matchPage.py
+++ matchPage.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from PIL import Image
 
+
 def show_match_page():
-    image = Image.open('banner/R.png')
-    st.image(image, use_column_width=True)
+    # image = Image.open('banner/R.png')
+    # st.image(image, use_column_width=True)
 
     st.header('View this weeks coffee chat match below')
 
-    classifyRoom()
--- optPage.py
+++ optPage.py
@@ -1,21 +1,23 @@
 import streamlit as st
 from PIL import Image
 
+
 def show_opt_page():
-    image = Image.open('banner/R.png')
-    st.image(image, use_column_width=True)
+    # image = Image.open('banner/R.png')
+    # st.image(image, use_column_width=True)
 
     st.header('Opt in / out of coffee chats')
 
     optOptions()
 
-def optOptions():
 
+def optOptions():
     nameList = ['Isobel Stewart', 'Alex Stone', 'Anitha Ramalingam', 'Chloe Gallacher', 'Chris Bellingham',
                 'Chris Magowan', 'Chris Nelis', 'Dave Edwards', 'Dave Miller', 'Elaine Porteous', 'Elisha Cooper',
                 'Emma Gillespie', 'Euan Cameron', 'Gillian McPhillips', 'Guy Chisholm', 'Jack Penman', 'Kirsty Purden',
                 'Linda Horsburgh', 'Ross McArthur', 'Ryan Sharkey', 'Senthamil Vijayakumar Pandiyan', 'Shaun Cooper',
-                'Suresh Natesan', 'David Ballantyne', 'Campbell Roberts', 'Kirsty ramsay', 'Jean Hezghia', 'Dylan Wright',
+                'Suresh Natesan', 'David Ballantyne', 'Campbell Roberts', 'Kirsty ramsay', 'Jean Hezghia',
+                'Dylan Wright',
                 'David Anderson', 'Tara McGonigle', 'Grant Stalker', 'Athos Georgiou'
                 ]
 
@@ -24,6 +26,3 @@ def optOptions():
 
     for name in nameList:
         st.radio('Select one ' + name, inOut)
-
-
-
