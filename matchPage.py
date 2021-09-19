99c7989c54edd8b4defd37cde791420b0c8ac7f2
CNelis <christopher.nelis@yahoo.com>
2021-09-19 13:50:26 +0100

    print matches

--- matchPage.py
+++ matchPage.py
@@ -1,3 +1,6 @@
+import itertools
+import random
+
 import streamlit as st
 from PIL import Image
 
@@ -7,4 +10,24 @@ def show_match_page():
     # st.image(image, use_column_width=True)
 
     st.header('View this weeks coffee chat match below')
+
+    nameList = ['Isobel Stewart', 'Alex Stone', 'Anitha Ramalingam', 'Chloe Gallacher', 'Chris Bellingham',
+                'Chris Magowan', 'Chris Nelis', 'Dave Edwards', 'Dave Miller', 'Elaine Porteous', 'Elisha Cooper',
+                'Emma Gillespie', 'Euan Cameron', 'Gillian McPhillips', 'Guy Chisholm', 'Jack Penman', 'Kirsty Purden',
+                'Linda Horsburgh', 'Ross McArthur', 'Ryan Sharkey', 'Senthamil Vijayakumar Pandiyan', 'Shaun Cooper',
+                'Suresh Natesan', 'David Ballantyne', 'Campbell Roberts', 'Kirsty ramsay', 'Jean Hezghia',
+                'Dylan Wright', 'David Anderson', 'Tara McGonigle', 'Grant Stalker', 'Athos Georgiou'
+                ]
+
+    def split_list(a_list):
+        half = len(a_list) // 2
+        return a_list[:half], a_list[half:]
+
+    a, b = split_list(nameList)
+
+    matches = random.sample(set(itertools.product(a, b)), 1)
+
+    st.write(matches)
+
+
 
